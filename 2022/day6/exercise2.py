5172cc9ecc554e46c81ceb0f4dc95a7915a96bad
Axel Köhler  | Infiniot <akohler@infiniot.nl>
2022-12-09 16:35:45 +0100

    Add day 6

---
+++ 2022/day6/exercise2.py
@@ -0,0 +1,30 @@
+def add_char_to_last_four(char, last_chars):
+    if len(last_chars) >= 14:
+        last_chars.pop(0)
+    last_chars.append(char)
+    return last_chars
+
+
+def is_fourteen_different_chars(chars):
+    if len(chars) < 14:
+        return False
+    
+    for i in range(len(chars) - 1):
+        for j in range( (i + 1), (len(chars)) ):
+            if chars[i] == chars[j]:
+                return False
+    
+    return True
+
+
+last_chars = []
+position = 0
+
+with open(r"input.txt", "r") as input:
+    while input:
+        char = input.read(1)
+        position += 1
+        last_chars = add_char_to_last_four(char, last_chars)
+        if is_fourteen_different_chars(last_chars):
+            print(position)
+            break
