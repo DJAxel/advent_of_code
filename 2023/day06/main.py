8c261b7e1b68842a62810dc6b1a4c84abe41f657
Axel Köhler <axel.kohler@elaad.nl>
2023-12-06 09:22:04 +0100

    Add 2023, day 06, ex 2

--- 2023/day06/main.py
+++ 2023/day06/main.py
@@ -4,25 +4,22 @@ from functools import reduce
 if __name__ == "__main__":
     with open("./2023/day06/input.txt", "r", encoding="utf8") as file:
         lines = [line.rstrip() for line in file]
-    races = list(
-        zip(
-            *[
-                [int(num) for num in sublist]
-                for sublist in [line.split(":")[1].split() for line in lines]
-            ]
-        )
-    )
 
-    total_winning_methods = []
+    values = [line.split(":")[1].split() for line in lines]
 
-    for time, highscore in races:
-        race_winning_methods = 0
-        for t in range(time):
-            speed = t + 1
-            travel_time = time - (t + 1)
-            score = speed * travel_time
-            if score > highscore:
-                race_winning_methods += 1
-        total_winning_methods.append(race_winning_methods)
+    p1_races = list(zip(*[[int(num) for num in sublist] for sublist in values]))
+    p2_races = [(tuple(int(reduce(lambda x, y: x + y, sublist)) for sublist in values))]
 
-    print(reduce(lambda x, y: x * y, total_winning_methods))
+    for pt, races in enumerate((p1_races, p2_races)):
+        total_winning_methods = []
+        for time, highscore in races:
+            race_winning_methods = 0
+            for t in range(time):
+                speed = t + 1
+                travel_time = time - (t + 1)
+                score = speed * travel_time
+                if score > highscore:
+                    race_winning_methods += 1
+            total_winning_methods.append(race_winning_methods)
+
+        print(f"Part {pt+1}: {reduce(lambda x, y: x * y, total_winning_methods)}")
