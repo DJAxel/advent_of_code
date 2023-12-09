5a9331bb16b69219689006efb98957b4191f5c79
Axel Köhler <axel.kohler@elaad.nl>
2023-12-09 19:39:32 +0100

    Add 2023, day09, ex 1

---
+++ 2023/day09/main.py
@@ -0,0 +1,20 @@
+def get_next_value(numbers: list[int]) -> int:
+    if numbers[:-1] == numbers[1:]:
+        return numbers[0]
+
+    diff_list: list[int] = []
+    for i in range(len(numbers) - 1):
+        diff_list.append(numbers[i + 1] - numbers[i])
+
+    return numbers[-1] + get_next_value(diff_list)
+
+
+if __name__ == "__main__":
+    with open("./2023/day09/input.txt", "r", encoding="utf8") as file:
+        part1 = 0
+        for line in file.readlines():
+            line = line.strip()
+            numbers = [int(n) for n in line.split()]
+            part1 += get_next_value(numbers)
+
+    print(part1)
