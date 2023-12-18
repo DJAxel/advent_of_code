c869af5b84e8d797b334652cd36bddee50350c96
Axel Köhler <axel.kohler@elaad.nl>
2023-12-18 15:44:55 +0100

    Add 2023, day 10, ex 2

--- 2023/day10/main.py
+++ 2023/day10/main.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+import re
 
 
 class Tile:
@@ -22,6 +23,10 @@ class Tile:
     @property
     def connects_right(self) -> bool:
         return self._symbol in ["-", "L", "F"]
+
+    @property
+    def symbol(self) -> str:
+        return self._symbol
 
     def connects_to_coords(self, row: int, col: int) -> bool:
         row_diff = row - self._row
@@ -65,29 +70,48 @@ def find_char(needle: str, grid: list[str]) -> tuple[int, int]:
     raise ValueError(f"{needle} is not present in the given grid")
 
 
+def is_in_loop(col: int, line: str) -> bool:
+    return len(re.findall(r"\||F-*J|L-*7", line[col:])) % 2 == 1
+
+
+def replace_tile_map(map: list[str], tile: Tile) -> list[str]:
+    map[tile.row] = (
+        map[tile.row][: tile.col] + tile.symbol + map[tile.row][tile.col + 1 :]
+    )
+    return map
+
+
 if __name__ == "__main__":
     with open("./2023/day10/input.txt", "r", encoding="utf8") as file:
         lines = [line.rstrip() for line in file.readlines()]
 
     # 1. Find S ((x, y))
-    (row, col) = find_char("S", lines)
+    (start_row, start_col) = find_char("S", lines)
 
     # 2. Find tiles connected to S
     connected_to_s: list[Tile] = []
     for diff in [1, -1]:
-        for compare_row, compare_col in ((row, col + diff), (row + diff, col)):
+        for compare_row, compare_col in (
+            (start_row, start_col + diff),
+            (start_row + diff, start_col),
+        ):
             compare_tile = Tile(
                 compare_row, compare_col, lines[compare_row][compare_col]
             )
-            if compare_tile.connects_to_coords(row, col):
+            if compare_tile.connects_to_coords(start_row, start_col):
                 connected_to_s.append(compare_tile)
 
     # 3. Find adjacent connecting pipes (should be two) until they have the same coordinate
-    start_tile = Tile(row, col, lines[row][col])
+    start_tile = Tile(
+        start_row,
+        start_col,
+        "L",  # This is hardcoded for my input.txt, might not work on yours
+    )
     connected: list[Tile] = connected_to_s
     next_connected: list[Tile] = []
     distance = 1
     visited: list[Tile] = [start_tile]
+    loop_map = replace_tile_map(["." * len(s) for s in lines], start_tile)
 
     while True:
         distance += 1
@@ -112,10 +136,22 @@ if __name__ == "__main__":
                     ):
                         next_connected.append(compare_tile)
                         visited.append(tile)
+                        loop_map = replace_tile_map(loop_map, tile)
 
         connected = next_connected
         next_connected = []
 
         if connected[0] == connected[1]:
-            print(distance)
+            loop_map = replace_tile_map(loop_map, connected[0])
             break
+
+    # Part 2
+    tiles_in_loop = 0
+    for row, line in enumerate(loop_map):
+        for col, _ in enumerate(line):
+            symbol = line[col]
+            if symbol == ".":
+                if is_in_loop(col, loop_map[row]):
+                    tiles_in_loop += 1
+
+    print(distance, tiles_in_loop)
